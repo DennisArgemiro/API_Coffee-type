c4923154531253be71d4f6fc373610e37397ce9a
DennisArgemiro <dennisarg2011@gmail.com>
2022-09-11 21:25:22 -0300

    tentativa de usar vercel 3

--- main.js
+++ main.js
@@ -3,6 +3,11 @@ const homeController = require("./controllers/home")
 const setTextController =require("./controllers/setText")
 
 const app = Express()
+const port  =  process.env.PORT | 3000
 
 app.use("/", homeController)
 app.use("/", setTextController)
+
+app.listen(port, ()=>{
+    console.log("Rodando na porta: " + port)
+})
