cc99f6ba388c6144539915d41728a933f374d6c8
DennisArgemiro <dennisarg2011@gmail.com>
2022-09-12 16:35:56 -0300

    api Remake v1.0

--- api/setText.js
+++ api/setText.js
@@ -1,23 +1,18 @@
-const express = require("express")
-const router = express.Router()
 const fs = require("fs");
 
-router.get("/sendText", async (req, res)=>{
-
-function getRandomText() {
-    const text = Math.floor(Math.random() *3);
-    return __dirname + "/../public/text/" + `text${text}.txt`;
-}
-
-const path = getRandomText();
-const data = fs.readFileSync(path, 'utf-8')
-
-const obj = JSON.parse('{"text":"'+data+'"}')
-
-res.json({
-    text: data,
-})
+module.exports = (req, res)=>{
+    function getRandomText() {
+        const text = Math.floor(Math.random() *3);
+        return __dirname + "/../public/text/" + `text${text}.txt`;
+    }
     
-}) 
-module.exports = router
+    const path = getRandomText();
+    const data = fs.readFileSync(path, 'utf-8')
+    
+    const obj = JSON.parse('{"text":"'+data+'"}')
+    
+    res.json({
+        text: data,
+    })
+}
  
