42ab22b412fa3cf9a2dc0305c3325905bbfa4d6f
DennisArgemiro <dennisarg2011@gmal.com>
2022-09-13 01:35:08 -0300

    Voltando ao normal

--- api/setText.js
+++ api/setText.js
@@ -7,7 +7,6 @@ module.exports = (req, res)=>{
     }
     const path = getRandomText();
     const data = fs.readFileSync(path, 'utf-8')
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.json({
         text: data,
     })
