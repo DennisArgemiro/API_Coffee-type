4d05201823f8787de63499a68806cfbb2eb7d82e
DennisArgemiro <dennisarg2011@gmail.com>
2022-09-13 01:30:06 -0300

    Implementando CORS

--- api/setText.js
+++ api/setText.js
@@ -5,10 +5,9 @@ module.exports = (req, res)=>{
         const text = Math.floor(Math.random() *3);
         return __dirname + "/../public/text/" + `text${text}.txt`;
     }
-    
     const path = getRandomText();
     const data = fs.readFileSync(path, 'utf-8')
-        
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.json({
         text: data,
     })
